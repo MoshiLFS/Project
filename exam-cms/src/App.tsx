dd0fdf310a8d7a6f9195f534bd2636b2159011b3
moshi <sh.1692839283.lfs@qq.com>
2019-09-04 00:33:36 +0800

    9.3

--- exam-cms/src/App.tsx
+++ exam-cms/src/App.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
-import '../src/static/App.min.css';
-import Header from './myTest/component/header/header'
-import Main from './myTest/component/main/main'
+import './App.css';
+import Login from './views/login'
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <Header/>
-        <Main/>
+        <Login/>
       </div>
     );
   }
---
+++ exam-cms/src/views/login/index.tsx
@@ -0,0 +1,13 @@
+
+import * as React from 'react'
+import Login from '../../component/login'
+
+class Login2 extends React.Component {
+    public render(){
+        return <>
+           <Login/>
+        </>
+    }
+}
+
+export default Login2;
