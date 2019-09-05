361db545cc1c1790fe46cfeaa464b98befdf1b67
ssn <1726863104@qq.com>
2019-09-05 16:25:44 +0800

    right

--- examproject/src/views/home/rank/index.tsx
+++ examproject/src/views/home/rank/index.tsx
@@ -1,12 +1,153 @@
 import * as React from 'react';
+import { Table } from 'antd';
+import { DndProvider, DragSource, DropTarget } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+import update from 'immutability-helper';
+import "./index.css";
 
-class Rank extends React.Component {
-   
-   public render() {
-        return (
-            <>Rank</>
+let dragingIndex = -1;
+interface Props {
+    isOver: any, connectDragSource: any, connectDropTarget: any, moveRow: any, style: any, className: any, index: number,
+}
+
+class BodyRow extends React.Component<Props>{
+    render() {
+        const { isOver, connectDragSource, connectDropTarget, moveRow, ...restProps } = this.props;
+        const style = { ...restProps.style, cursor: 'move' };
+
+        let { className } = restProps;
+        if (isOver) {
+            if (restProps.index > dragingIndex) {
+                className += ' drop-over-downward';
+            }
+            if (restProps.index < dragingIndex) {
+                className += ' drop-over-upward';
+            }
+        }
+
+        return connectDragSource(
+            connectDropTarget(<tr {...restProps} className={className} style={style} />),
         );
     }
 }
 
+const rowSource = {
+    beginDrag(props: { index: number; }) {
+        dragingIndex = props.index;
+        return {
+            index: props.index,
+        };
+    },
+};
+
+const rowTarget = {
+    drop(props: { index: any; moveRow: (arg0: any, arg1: any) => void; }, monitor: { getItem: { (): { index: any; }; (): { index: any; }; }; }) {
+        const dragIndex = monitor.getItem().index;
+        const hoverIndex = props.index;
+
+        // Don't replace items with themselves
+        if (dragIndex === hoverIndex) {
+            return;
+        }
+
+        // Time to actually perform the action
+        props.moveRow(dragIndex, hoverIndex);
+
+        // Note: we're mutating the monitor item here!
+        // Generally it's better to avoid mutations,
+        // but it's good here for the sake of performance
+        // to avoid expensive index searches.
+        monitor.getItem().index = hoverIndex;
+    },
+};
+
+const DragableBodyRow = DropTarget('row', rowTarget, (connect, monitor) => ({
+    connectDropTarget: connect.dropTarget(),
+    isOver: monitor.isOver(),
+}))(
+    DragSource('row', rowSource, connect => ({
+        connectDragSource: connect.dragSource(),
+    }))(BodyRow),
+);
+
+const columns = [
+    {
+        title: '类型ID',
+        dataIndex: 'name',
+        key: 'name',
+    },
+    {
+        title: '类型名称',
+        dataIndex: 'age',
+        key: 'age',
+    },
+    {
+        title: '操作',
+        dataIndex: '操作',
+        key: '操作',
+    },
+];
+
+class Rank extends React.Component {
+    state = {
+        data: [
+            {
+                key: '1',
+                name: 'John Brown',
+                age: '简答题',
+                address: 'New York No. 1 Lake Park',
+            },
+            {
+                key: '2',
+                name: 'Jim Green',
+                age: '代码阅读题',
+                address: 'London No. 1 Lake Park',
+            },
+            {
+                key: '3',
+                name: 'Joe Black',
+                age: '代码补全',
+                address: 'Sidney No. 1 Lake Park',
+            },
+        ],
+    };
+
+    components = {
+        body: {
+            row: DragableBodyRow,
+        },
+    };
+
+    moveRow = (dragIndex: number, hoverIndex: number) => {
+        const { data } = this.state;
+        const dragRow = data[dragIndex];
+
+        this.setState(
+            update(this.state, {
+                data: {
+                    $splice: [[dragIndex, 1], [hoverIndex, 0, dragRow]],
+                },
+            }),
+        );
+    };
+
+    render() {
+        return (
+            <DndProvider backend={HTML5Backend}>
+            <p>试题分类</p>
+                <Table
+                    columns={columns}
+                    dataSource={this.state.data}
+                    components={this.components}
+                    onRow={(record, index) => ({
+                        index,
+                        moveRow: this.moveRow,
+                    })}
+                />
+            </DndProvider>
+        );
+    }
+}
 export default Rank;
+
+
