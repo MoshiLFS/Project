48c3bb602625979e26e2470e107121e0c2d248c5
ssn <1726863104@qq.com>
2019-09-05 11:28:49 +0800

    yemian

--- examproject/src/views/home/index.tsx
+++ examproject/src/views/home/index.tsx
@@ -27,6 +27,7 @@ class Home extends React.Component<Props> {
                 <Layout>
                     <Header>
                     <div className="imgs"><img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1551624718911&di=4a7004f8d71bd8da84d4eadf1b59e689&imgtype=0&src=http%3A%2F%2Fimg105.job1001.com%2Fupload%2Falbum%2F2014-10-15%2F1413365052_95IE3msH.jpg" alt=""/></div>
+                    <div>孙少娜</div>
                     </Header>
                     <Layout>
                         <Sider>
