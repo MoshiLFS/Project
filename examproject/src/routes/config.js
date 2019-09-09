f8815df605856440976e5667300e0911c61ea57b
moshi <sh.1692839283.lfs@qq.com>
2019-09-09 14:31:33 +0800

    lfs

--- examproject/src/routes/config.js
+++ examproject/src/routes/config.js
@@ -1,18 +1,3 @@
-// import Home from '../views/home/index.tsx';
-// import Login from "../views/login/index.tsx";
-// import Rank from '../views/home/question_management/rank';
-// import Add from '../views/home/question_management/add';
-// import Check from '../views/home/question_management/check';
-// const routes = [
-//     {path: '/login', compoennt: Login} ,
-//     {path: '/home', compoennt: Home, children: [
-//         {path: '/home/rank', compoennt: Rank},
-//         {path: '/home/add', compoennt: Add},
-//         {path: '/home/check', compoennt: Check},
-//     ]},
-//     {path: '/', redirect: '/login'}
-// ]
-// export default routes;
 
 import Home from '../views/home';
 import Login from "../views/login";
